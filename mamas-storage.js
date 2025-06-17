c19fed7e1e199b87674365227b385260af6001dc
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-17 16:54:11 +0300

    added InMemorySharedStorage class

--- mamas-storage.js
+++ mamas-storage.js
@@ -38,8 +38,41 @@ class InMemoryStorage {
     }
 }
 
-const storage = new InMemoryStorage();
-storage.create('users', {'username': 'eden', 'password': 'password123'});
-console.log(storage);
-storage.remove('users', item => item.username === 'eden');
-console.log(storage);
+class InMemorySharedStorage {
+    static sharedStorage = {};
+
+    constructor() {
+        this.storage = InMemorySharedStorage.sharedStorage;
+    }
+
+    create(collectionName, item) {
+        if (!this.storage[collectionName]) {
+            this.storage[collectionName] = [];
+        }
+        const itemWithId = {_id: uuidv4(), ...item};
+        this.storage[collectionName].push(itemWithId);
+        return itemWithId;
+    }
+
+    find(collectionName, findFunc) {
+        return this.storage[collectionName].filter(findFunc);
+    }
+
+    where(collectionName, where) {
+        return this.storage[collectionName].filter((item) => {
+            Object.keys(where).every((key) => item[key] == where[key]);
+        });
+    }
+
+    remove(collectionName, findFunc) {
+        const removedItems = [];
+        this.storage[collectionName] = this.storage[collectionName].filter(item => {
+            if (findFunc(item)) {
+                removedItems.push(item);
+                return false;
+            }
+            return true;
+        });
+        return removedItems;
+    }
+}
