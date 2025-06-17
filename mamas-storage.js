2a69e9cb6e8f73cf1d9621b8317c430ab74a52ee
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-17 16:10:48 +0300

    finished InMemoryStorage class

--- mamas-storage.js
+++ mamas-storage.js
@@ -1,4 +1,45 @@
 const { v4: uuidv4 } = require('uuid');
 const lodash = require('lodash');
-console.log(uuidv4());
-console.log(lodash.compact([false, '1']))
+
+class InMemoryStorage {
+    constructor() {
+        this.storage = {};
+    }
+
+    create(collectionName, item) {
+        if (!this.storage[collectionName]) {
+            this.storage[collectionName] = [];
+        }
+        const itemWithId = {_id: uuidv4(), ...item};
+        this.storage[collectionName].push(itemWithId);
+        return itemWithId;
+    }
+
+    find(collectionName, findFunc) {
+        return this.storage[collectionName].filter(findFunc);
+    }
+
+    where(collectionName, where) {
+        return this.storage[collectionName].filter((item) => {
+            Object.keys(where).every((key) => item[key] == where[key]);
+        });
+    }
+
+    remove(collectionName, findFunc) {
+        const removedItems = [];
+        this.storage[collectionName] = this.storage[collectionName].filter(item => {
+            if (findFunc(item)) {
+                removedItems.push(item);
+                return false;
+            }
+            return true;
+        });
+        return removedItems;
+    }
+}
+
+const storage = new InMemoryStorage();
+storage.create('users', {'username': 'eden', 'password': 'password123'});
+console.log(storage);
+storage.remove('users', item => item.username === 'eden');
+console.log(storage);
